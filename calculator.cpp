964b24ac75020e2e49b924fa555bc993b9bd6772
Oleg Katasonov <oleg.katasonov@globallogic.com>
2020-07-20 14:50:23 +0300

    Updated Calculator
    - Added Div and Round methods
    
    Signed-off-by: Oleg Katasonov <oleg.katasonov@globallogic.com>

--- calculator.cpp
+++ calculator.cpp
@@ -14,3 +14,20 @@ int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
 }
+
+int Calculator::Div(double a, double b)
+{
+	if (b!=0)
+		return Mull(a, 1.0/b);
+}
+
+int Calculator::Round(double a)
+{
+	if ((a - (int)a)>0.5)
+	{
+		return (a + 0.5);
+	}else
+	{
+		return a;
+	}
+}
